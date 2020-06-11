ed7d004831e15fc98327816996b770fc1621b503
Sebastian Sulinski <info@sebastiansulinski.co.uk>
2020-06-11 12:07:03 +0100

    Re-organised imports

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue';
-import App from './App.vue';
 import VueCompositionApi from '@vue/composition-api';
+
 Vue.use(VueCompositionApi);
 
-import { createElement } from '@vue/composition-api';
+import App from './App.vue';
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: () => createElement(App),
+  render: h => h(App),
 }).$mount('#app');
